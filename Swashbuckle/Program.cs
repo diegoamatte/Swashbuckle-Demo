c8da0c774b7fceb19c4c26040f13a6ee27034bd4
Diego Amatte <amattediegoomar@gmail.com>
2022-04-04 13:41:44 -0300

    Add Nswag

--- Swashbuckle/Program.cs
+++ Swashbuckle/Program.cs
@@ -1,12 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddControllers()
+    .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
-builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+
+
+
+//Nswag
+builder.Services.AddOpenApiDocument();
+
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseOpenApi();
+    app.UseSwaggerUi3();
+}
 
 app.UseHttpsRedirection();
 
